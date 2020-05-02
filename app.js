ef47cf202915b3c88b172f1217dbe869f8004d13
vidisha444 <vidisha444@gmail.com>
2020-05-02 18:35:34 +0530

    Check 4

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ var indexRouter = require('./routes/index');
 
 var app = express();
 
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
