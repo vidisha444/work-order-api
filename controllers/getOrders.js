4658c815d796f5677344219e51ec5c02060da71e
Usha Gupta <ushagupta@Ushas-MacBook-Air.local>
2020-05-03 00:39:18 +0530

    change select query

--- controllers/getOrders.js
+++ controllers/getOrders.js
@@ -3,13 +3,13 @@
 
 function getOrders(req, res) {
     let flag = req.body.flag
-    let query = 'select * from orders status = ?'
+    let query = 'select * from orders where status = ?'
     connectDatabase(query, [flag])
         .then((rows) => {
             if (!_.isEmpty(rows)) {
                 res.status(200).send({ code: 200, message: 'Success', data: rows })
             } else {
-                res.status(400).send({ code: 400, message: "Failed db action" })
+                res.status(400).send({ code: 400, message: "No oreders available" })
             }
         }).catch((err) => {
             res.status(500).send({ code: 500, msg: "Can't connect to db" })
